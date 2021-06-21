78f73dd40117ad6c2330ebc862e813edba26aa16
Aritana Noara <0483369+aritana@users.noreply.github.com>
2021-06-21 16:37:50 -0300

    exercicio 2: done

--- exercicio2.js
+++ exercicio2.js
@@ -2,49 +2,72 @@
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
 
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/osiris';
-const imagens = [
-    {
-      arquivo: 'modelo-artistico-da-sonda.webp',
-      descricao: 'Imagem em 3D mostrando a sonda tocando um asteróide. ' +
-        'A sonda tem forma de caixa com dois painéis solares em suas ' +
-        'laterais e, na parte de baixo, um pequeno braço sai e encosta ' +
-        'no asteróide'
+const imagens = [{
+        arquivo: 'modelo-artistico-da-sonda.webp',
+        descricao: 'Imagem em 3D mostrando a sonda tocando um asteróide. ' +
+            'A sonda tem forma de caixa com dois painéis solares em suas ' +
+            'laterais e, na parte de baixo, um pequeno braço sai e encosta ' +
+            'no asteróide'
     },
     {
-      arquivo: 'lancamento-atlas-v-em-2017.webp',
-      descricao: 'Foto de foguete na vertical no momento de seu ' +
-        'lançamento. Ele possui bandeiras da NASA, dos EUA e da empresa ' +
-        'ULA e está com o motor sendo ligado, saindo fogo e fumaça'
+        arquivo: 'lancamento-atlas-v-em-2017.webp',
+        descricao: 'Foto de foguete na vertical no momento de seu ' +
+            'lançamento. Ele possui bandeiras da NASA, dos EUA e da empresa ' +
+            'ULA e está com o motor sendo ligado, saindo fogo e fumaça'
     },
     {
-      arquivo: 'distanciamento-da-sonda-da-terra.webp',
-      descricao: 'Foto do espaço quase toda preta mostrando a Terra bem ' +
-        'pequena no canto esquerdo e, no canto direito, a lua ainda menor'
+        arquivo: 'distanciamento-da-sonda-da-terra.webp',
+        descricao: 'Foto do espaço quase toda preta mostrando a Terra bem ' +
+            'pequena no canto esquerdo e, no canto direito, a lua ainda menor'
     },
     {
-      arquivo: 'aproximacao-da-sonda-ao-bennu-em-2018.webp',
-      descricao: 'Foto do espaço mostrando o asteróide Bennu que tem ' +
-        'a forma de um paralelepípedo com base losangular aproximadamente ' +
-        'e a superfície acinzentada'
+        arquivo: 'aproximacao-da-sonda-ao-bennu-em-2018.webp',
+        descricao: 'Foto do espaço mostrando o asteróide Bennu que tem ' +
+            'a forma de um paralelepípedo com base losangular aproximadamente ' +
+            'e a superfície acinzentada'
     },
     {
-      arquivo: 'local-de-pouso.webp',
-      descricao: 'Foto do local de pouso da sonda Osiris-Rex no ' +
-        'asteróide Bennu mostrando uma pequena clareira em um ' +
-        'terreno bem acidentado, com muitas pedras. No centro há ' +
-        'um desenho no formato da sonda'
+        arquivo: 'local-de-pouso.webp',
+        descricao: 'Foto do local de pouso da sonda Osiris-Rex no ' +
+            'asteróide Bennu mostrando uma pequena clareira em um ' +
+            'terreno bem acidentado, com muitas pedras. No centro há ' +
+            'um desenho no formato da sonda'
     },
     {
-      arquivo: 'touch-and-go-e-detritos-suspensos-em-2020.webp',
-      descricao: 'Foto do asteróide Bennu com vários regolitos (pedras) ' +
-        'sendo suspensos após a sonda ter encostado para coletar amostras ' +
-        'da sua superfície'
+        arquivo: 'touch-and-go-e-detritos-suspensos-em-2020.webp',
+        descricao: 'Foto do asteróide Bennu com vários regolitos (pedras) ' +
+            'sendo suspensos após a sonda ter encostado para coletar amostras ' +
+            'da sua superfície'
     },
     {
-      arquivo: 'sonda-distanciando-e-voltando-para-terra.webp',
-      descricao: 'Foto do asteróide Bennu com ele retratado em ' +
-        'menos da metade e com baixa iluminação, mostrando sua superfície ' +
-        'acinzentada'
+        arquivo: 'sonda-distanciando-e-voltando-para-terra.webp',
+        descricao: 'Foto do asteróide Bennu com ele retratado em ' +
+            'menos da metade e com baixa iluminação, mostrando sua superfície ' +
+            'acinzentada'
     }
-  ];
+];
 
+
+const fotoAtualGaleriaEl = document.querySelector('#slide');
+const buttonAnteriorEl = document.querySelector('#anterior');
+const buttonProximoEl = document.querySelector('#proximo');
+let currentImage = 0;
+
+
+buttonAnteriorEl.addEventListener('click', function() {
+    if (currentImage - 1 < 0) {
+        currentImage = 6;
+    } else {
+        currentImage = (--currentImage) % 7;
+    }
+    nextImage();
+});
+buttonProximoEl.addEventListener('click', function() {
+    currentImage = (++currentImage) % 7;
+    nextImage();
+});
+
+function nextImage() {
+    fotoAtualGaleriaEl.src = servidorDasImagens + '/' + imagens[currentImage].arquivo;
+    fotoAtualGaleriaEl.alt = imagens[currentImage].descricao;
+}
