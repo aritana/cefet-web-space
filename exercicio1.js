25c0e473001c5cbf681ef2c53b8b50d6a858764d
Aritana Noara <0483369+aritana@users.noreply.github.com>
2021-06-21 12:14:44 -0300

    exercicio 1: done

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,21 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const botaoCalcularFGravidadeEl = document.querySelector('#calcular');
+const constanteEl = document.querySelector('#constante');
+const massa1El = document.querySelector('#massa1');
+const massa2El = document.querySelector('#massa2');
+const distanciaEl = document.querySelector('#distancia');
+const resultadoEl = document.querySelector('#resultado');
+
+
+botaoCalcularFGravidadeEl.addEventListener('click', calcularFGravidade);
+
+function calcularFGravidade() {
+    const G = constanteEl.value;
+    const M1 = massa1El.value;
+    const M2 = massa2El.value;
+    const d = distanciaEl.value;
+    const forcaGravitacional = G * M1 * M2 / d ** 2;
+    resultadoEl.value = forcaGravitacional;
+}
